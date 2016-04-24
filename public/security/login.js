3eca5032747d5a74d1efb4556c37de63d32d2c62
c c <c c>
2016-04-24 11:24:41 +0800

    add auth fb and anoymous

---
+++ public/security/login.js
@@ -0,0 +1,22 @@
+angular.module('app').component('login',{
+	templateUrl:'/security/login.html',
+	controller:function(auth,$location){
+		this.anonLogin=function(){
+			auth.$authAnonymously().then(function(){
+				$location.path('/home');
+			}).catch((function(err){
+				this.errorMessage=err.code;
+				console.log('err'+err);
+			}).bind(this))
+		}
+
+		this.fbLogin=function(){
+			auth.$authWithOAuthPopup("facebook").then(function(){
+				$location.path('/home');
+			}).catch((function(err){
+				this.errorMessage=err.code;
+				console.log('err'+err);
+			}).bind(this))
+		}
+	}
+})
