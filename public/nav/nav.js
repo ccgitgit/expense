2978fc1f6b4346a6472aa17c628585160737a1d2
ccgitgit <oldtie@icloud.com>
2016-04-25 00:04:53 +0800

    firebase obj and three-way-binding

---
+++ public/nav/nav.js
@@ -0,0 +1,11 @@
+angular.module('app').component('nav',{
+	templateUrl:'/nav/nav.html',
+	controller:function($firebaseObject,fbRef){
+		this.loaded=false;
+		this.userPreferences=$firebaseObject(fbRef.getPreferencesRef());
+		this.userPreferences.$loaded().then((function(data){
+			this.loaded=true;
+			this.darktheme=this.userPreferences.theme==='dark';
+		}).bind(this))
+	}
+})
--- public/security/login.js
+++ public/security/login.js
@@ -1,6 +1,11 @@
 angular.module('app').component('login',{
 	templateUrl:'/security/login.html',
+	bindings:{
+		currentAuth:'='
+	},
 	controller:function(auth,$location){
+		this.loggedIn=!!this.currentAuth;
+
 		this.anonLogin=function(){
 			auth.$authAnonymously().then(function(){
 				$location.path('/home');
